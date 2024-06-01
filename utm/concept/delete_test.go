76e1c1a4f436b5f1c55808ca7654d47a9100cefa
keisuke inazaki <keisuke.inazaki@gmail.com>
2024-06-01 21:59:45 +0900

    add delete_test.go

---
+++ utm/concept/delete_test.go
@@ -0,0 +1,72 @@
+package concept
+
+import (
+	"github.com/inazak/turing-machine/tm"
+	"github.com/inazak/turing-machine/tmbuilder"
+	"strings"
+	"testing"
+)
+
+func verify(m *tm.Machine, want string) (string, bool) {
+	runes := m.Tape()
+	for i, r := range runes {
+		if r == tm.BLANK {
+			runes[i] = '_'
+		}
+	}
+	s := strings.Trim(string(runes), "_")
+	return s, s == want
+}
+
+func TestDelete(t *testing.T) {
+	tape := "101101M1111101101"
+	want := "101101M1101"
+
+	// before
+	// ------
+	// 101101M1111101101
+	// ^HEAD  |    |
+	//        +----+
+	//        delete
+	//
+	// after
+	// -----
+	// 101101M1101
+	//       ^HEAD
+	//
+	app := `
+		state search-mark-0 match 0     write 0     move right next search-mark-0
+		                    match 1     write 1     move right next search-mark-0
+		                    match M     write M     move right next move-rightest-0
+
+		state move-rightest-0 match 0     write 0     move right next move-rightest-0
+		                      match 1     write 1     move right next move-rightest-0
+		                      match blank write blank move left  next del-start
+
+		state del-start match 0     write blank move left next del-step-0
+		                match 1     write blank move left next del-step-1
+
+		state del-step-0 match 0     write 0     move left next del-step-0
+		                 match 1     write 0     move left next del-step-1
+		                 match M     write M     move left next finish
+
+		state del-step-1 match 0     write 1     move left next del-step-0
+		                 match 1     write 1     move left next del-step-1
+		                 match M     write M     move right next move-rightest-0
+
+		#start point
+		begin search-mark-0
+	`
+
+	b := tmbuilder.New()
+	ma, err := b.Build(tape, app)
+	if err != nil {
+		t.Fatalf("builder.Build error, got=%s", err)
+	}
+
+	ma.Run()
+
+	if got, ok := verify(ma, want); !ok {
+		t.Errorf("unexpected tape dump, want=%s, but got=%s", want, got)
+	}
+}
