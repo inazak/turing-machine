666df84f45282a2ea51524e130c9a95c3d7ae028
keisuke inazaki <keisuke.inazaki@gmail.com>
2024-06-04 21:12:19 +0900

    add tapedump()

--- utm/concept/testhelper.go
+++ utm/concept/testhelper.go
@@ -5,13 +5,17 @@ import (
 	"strings"
 )
 
-func verify(m *tm.Machine, want string) (string, bool) {
+func tapedump(m *tm.Machine) string {
 	runes := m.Tape()
 	for i, r := range runes {
 		if r == tm.BLANK {
 			runes[i] = '_'
 		}
 	}
-	s := strings.Trim(string(runes), "_")
+	return strings.Trim(string(runes), "_")
+}
+
+func verify(m *tm.Machine, want string) (string, bool) {
+	s := tapedump(m)
 	return s, s == want
 }
